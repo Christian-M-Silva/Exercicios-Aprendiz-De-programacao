61834caa210ea0ee7d1c06f81c7546abd9ba8242
Christian-M-Silva <80538257+Christian-M-Silva@users.noreply.github.com>
2021-11-17 16:54:03 -0300

    Teste com variáveis

---
+++ JavaScript/Variaveis/funcao.js
@@ -0,0 +1,9 @@
+function pegar(){
+    let nome= prompt("Qual é seu nome?");
+
+    let idade= prompt("Qual sua idade?");
+
+    let cidade= prompt("Qual sua cidade?");
+
+    document.write("O nome é " + nome + " a idade é " + idade + " e a cidade é " + cidade);
+}
