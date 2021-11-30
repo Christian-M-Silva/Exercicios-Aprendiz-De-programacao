a19f866eb778d1f4de4ac1abf28a0c5f7255730f
Christian-M-Silva <80538257+Christian-M-Silva@users.noreply.github.com>
2021-11-30 17:37:32 -0300

    Primeiros passos
    Se tornando um Javeiro profissa

---
+++ Java/Exercicio Java do DevDojo/Hello World/HelloWorld.java
@@ -0,0 +1,6 @@
+public class HelloWorld{
+	public static void main (String[] args){
+		System.out.println("Hello World!!!");
+		System.out.println("Esse codigo eu fiz de forma manual, no Bloco de notas");
+	}
+}
