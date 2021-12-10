b759337687f37cc3e5723225b8977745098382c6
Christian-M-Silva <80538257+Christian-M-Silva@users.noreply.github.com>
2021-12-10 16:14:30 -0300

    Exercicio React

--- Node/Projetos/aula1/src/App.js
+++ Node/Projetos/aula1/src/App.js
@@ -1,22 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './Header';
+
+function clicando(){
+  alert ('Você clicou no botão!')
+}
+
+function douglas(){
+  alert ('Eu sou o Douglas')
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        
+        <Header/>
+        
+        <button onClick= {clicando}>Clique here</button>
+        <button onClick= {douglas}>Você não é o Douglas</button>
       </header>
     </div>
   );
