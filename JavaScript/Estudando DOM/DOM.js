937c500e2cfe5e1659dfadd7e51a9f171d9a13f1
Christian-M-Silva <hoodprogrammer@gmail.com>
2022-02-19 07:29:52 -0300

    Estudando DOM

---
+++ JavaScript/Estudando DOM/DOM.js
@@ -0,0 +1,16 @@
+const element = document.querySelector('body')
+
+/*
+element.style.border= "1px solid red"
+
+console.log(element.style.border)
+
+element.classList.add('red', 'font')
+
+element.classList.remove('red')
+
+element.classList.toggle('red')
+
+*/
+
+console.log(element.lastElementChild)
