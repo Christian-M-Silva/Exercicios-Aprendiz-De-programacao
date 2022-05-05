a60aa59783f03895454e07f5a35f53e9c8cd75ac
Christian-M-Silva <hoodprogrammer@gmail.com>
2022-05-05 12:59:31 -0300

    Update aprendizado

---
+++ React/hello-world/src/App.tsx
@@ -0,0 +1,9 @@
+export function App() {
+
+  return(
+    <div className="flex h-screen px-3 justify-center items-center	flex-col gap-1 bg-gradient-to-b from-indigo-500">
+      <h1 className="text-4xl">Hello World!!!</h1>
+      <p className="font-mono">My name is Christian and i love programming and developing applications and this is my first ReactJS project using TypeScript and Tailwind </p>
+    </div>
+  )
+}
